5cebc6464d014a6c2c377ab39856f1574bae16c5
Adam <adamllryan@gmail.com>
2022-04-20 10:34:39 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import serial
+import tkinter as tk
+
+s = serial.Serial('/dev/cu.usbserial-1330')
+
+while (s.isOpen()):
+    line = str(s.readline())
+
+    runtime = line[2:line.index(' ')]
+    last_runtime = line[line.index(' '):len(line) - 5]
+    if runtime != '0':
+        print("Runtime: ", runtime, "s Last: ", last_runtime, "s")
