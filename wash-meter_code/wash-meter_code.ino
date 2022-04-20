b9ab327259d9804d01cf6e5767f7d7e264cb24cd
Adam <adamllryan@gmail.com>
2022-04-20 09:39:33 -0400

    Update wash-meter_code.ino

--- wash-meter_code/wash-meter_code.ino
+++ wash-meter_code/wash-meter_code.ino
@@ -1,23 +1,28 @@
-
-class machine {
-  bool state = false;
-  //thresholod to determine if on or off
-  int threshold = 1;
-  bool isOn() {
-    return state;
-  }
-  void setThreshold(int i) {
-    this->threshold = i;
-  }
-};
-
 void setup() {
-  int AIN0 = 115;
-  pinMode(AIN0, INPUT);
-  machine* m = new machine();
+  // put your setup code here, to run once:
+  pinMode(A1, INPUT);
+  Serial.begin(9600);
 }
-
+int runtime = 0;
+int last_runtime = 0;
+bool isRunning = false;
+int max_voltage = 2;
+int on_voltage = 1;
+int threshold = 1024/max_voltage*on_voltage;
 void loop() {
+  // put your main code here, to run repeatedly:
+  delay(1000);
+  isRunning = (analogRead(A1)>threshold);
+  if (isRunning) {
+    runtime++;
+    } else {
+      if (runtime !=0)
+        last_runtime = runtime;
+      runtime = 0;
+      
+      }
   
-  
+  Serial.print(runtime);
+  Serial.print(" ");
+  Serial.println(last_runtime);
 }
