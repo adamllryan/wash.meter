80a00308e24ef394b7b53505e8f6dde94095bd28
Adam <adamllryan@gmail.com>
2022-04-28 16:44:17 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ s = serial.Serial('/dev/cu.usbserial-1330')
 
 while (s.isOpen()):
     line = str(s.readline())
-
     runtime = line[2:line.index(' ')]
     last_runtime = line[line.index(' '):len(line) - 5]
     if runtime != '0':
